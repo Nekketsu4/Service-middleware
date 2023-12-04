e943e06649c9dbe7df57608f7f6ddf204ba359e6
Nekketsu4 <101641229+Nekketsu4@users.noreply.github.com>
2023-12-05 01:21:11 +0300

    fixed child_classes to services

--- api_route/views.py
+++ api_route/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from api_route.child_classes.children import *
+from api_route.services.children import *
 from api_route.serializers import RegAuthSerializer
 
 
